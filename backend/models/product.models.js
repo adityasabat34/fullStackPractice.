34892f8fa0a8673fbf3561308472846db426a78f
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-10-25 19:04:20 +0530

    productSchema and categorySchema created for data model structure

---
+++ backend/models/product.models.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    productImage: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
