22bf796fc06fd32f2642ca2590209a345d8cad74
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-11-14 22:05:40 +0530

    bcrypt hashing password done

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,25 +1,64 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema(
+const userSchema = new Schema(
   {
-    username: {
+    userName: {
       type: String,
       required: true,
-      lowercase: true,
       unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
+      trim: true,
     },
-    password: {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
       type: String,
       required: true,
     },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
   },
+
   { timestamps: true }
 );
+
+userSchema.methods.isPasswordCorrect = async function () {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 export const User = mongoose.model("User", userSchema);
