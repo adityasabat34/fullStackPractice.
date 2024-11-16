5805b6a381b42d78af50848aff42b5c01a1c08f5
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-11-16 20:33:50 +0530

    video model done and also generate and refresh token created for users

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -50,15 +50,38 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-userSchema.methods.isPasswordCorrect = async function () {
-  return await bcrypt.compare(password, this.password);
+userSchema.methods.isPasswordCorrect = async function (enterPassword) {
+  return await bcrypt.compare(enterPassword, this.password);
 };
 
 userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) return next();
+  if (!this.isModified("password")) return next();
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
