0643b01369b77f97ad51ce581d59cc0e1c7df265
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-11-22 14:48:48 +0530

    some change and arrange of file and code

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,19 @@
-// import express from "express";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-// const app = express();
+const app = express();
 
-// export default app;
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // origin means where the request is coming from
+    credentials: true,
+  })
+);
+
+app.use(express.json()); // for requesting body parsing
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // Returns middleware that only parses urlencoded bodies
+app.use(cookieParser()); // Parses cookies from incoming requests and makes them available in req.cookies
+app.use(express.static("public")); // This is a built-in middleware function in Express. It serves static files and is based on serve-static.
+
+export { app };
--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,31 @@
-import express from "express";
+import { app } from "./app.js";
 import connectDB from "./src/config/db.js";
 import dotenv from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 
 dotenv.config({
   path: "./.env", // path is the location of the env file
 });
 
-connectDB();
+(async () => {
+  try {
+    await connectDB();
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`⚙️ Server is running at port: ${port}`);
+    });
+  } catch (err) {
+    console.error("MongoDB connection failed", err);
+    process.exit(1);
+  }
+})();
+// another sample for DB connection
 
-const app = express();
-
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN, // origin means where the request is coming from
-    credentials: true,
-  })
-);
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.json()); // Parses incoming request bodies with JSON payloads and converts them to JavaScript objects
-// that can be accessed via req.body.
-app.use(cookieParser()); // // Parses cookies from incoming requests and makes them available in req.cookies
-const port = process.env.PORT || 5500;
-
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
-app.get("/api/info", (req, res) => {
-  const contactInfo = [
-    { id: 1, name: "Aditya", age: 30, lastName: "Sabat" },
-    { id: 2, name: "Aditya", age: 30, lastName: "Sabat" },
-    { id: 3, name: "Aditya", age: 30, lastName: "Sabat" },
-  ];
-  // res.json(contactInfo)
-  res.send(contactInfo);
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening on ${port} `);
-});
+// connectDB()
+// .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!! ", err);
+// })
