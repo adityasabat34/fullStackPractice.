e05e6e1476ffa25f571f0b756c50db244707411c
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-12-07 17:23:49 +0530

    coverImage update and avatarImage update routes set up done.

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -7,6 +7,8 @@ import {
   changeUserPassword,
   getCurrentUser,
   updateAccountDetails,
+  updateAvatarImage,
+  updateCoverImage,
 } from "../controllers/userController.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middlware.js";
@@ -35,5 +37,11 @@ router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeUserPassword);
 router.route("/users").get(verifyJWT, getCurrentUser);
 router.route("/update-account").post(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar-image")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatarImage);
+router
+  .route("/cover-image")
+  .post(verifyJWT, upload.single("coverImage"), updateCoverImage);
 
 export default router;
